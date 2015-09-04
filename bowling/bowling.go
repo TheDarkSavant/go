fa138a22c7181475043eacdd3848cb2c97d8c402
Curtis Cooley <curtis@thecooleys.org>
2015-09-04 14:51:15 -0700

    Test spare

--- bowling/bowling.go
+++ bowling/bowling.go
@@ -6,18 +6,27 @@ type game struct {
 }
 
 type frame struct {
-	firstBall  int
-	secondBall int
+	rolls []int
 }
 
+var rolls = make([]int, 22)
+var rollIndex = 0
+
 func (g *game) ScoreFrame(ballOne int, ballTwo int) {
-	g.frames = append(g.frames, frame{ballOne, ballTwo})
+	rolls[rollIndex] = ballOne
+	rolls[rollIndex+1] = ballTwo
+	g.frames = append(g.frames, frame{rolls[rollIndex : rollIndex+2]})
+	rollIndex = rollIndex + 2
 }
 
 func (g game) GameScore() int {
 	var score int
 	for _, frame := range g.frames {
-		score = score + frame.firstBall + frame.secondBall
+		score = score + frame.FrameScore()
 	}
 	return score
+}
+
+func (f frame) FrameScore() int {
+	return f.rolls[0] + f.rolls[1]
 }
--- bowling/bowling_test.go
+++ bowling/bowling_test.go
@@ -6,14 +6,23 @@ import (
 )
 
 func TestScoreAllOnes(t *testing.T) {
-	g := game{frames: make([]frame, 10)}
+	g := game{frames: make([]frame, 0)}
 	g.ScoreFrame(1, 1)
 	assert.Equal(t, 2, g.GameScore())
 }
 
 func TestTwoFrames(t *testing.T) {
-	g := game{frames: make([]frame, 10)}
+	g := game{frames: make([]frame, 0)}
 	g.ScoreFrame(2, 3)
 	g.ScoreFrame(2, 5)
 	assert.Equal(t, 12, g.GameScore())
 }
+
+/*
+func TestSpare(t *testing.T) {
+	g := game{frames: make([]frame, 10)}
+	g.ScoreFrame(5, 5)
+	g.ScoreFrame(8, 0)
+	assert.Equal(t, 26, g.GameScore())
+}
+*/
